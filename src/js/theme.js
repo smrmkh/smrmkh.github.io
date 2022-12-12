50855400d29893841658af96c4b558d8f9ec9455
smrmkh <smrmkh.2005@gmail.com>
2022-12-12 15:51:26 +0330

    lightbox for dektop added

--- src/js/theme.js
+++ src/js/theme.js
@@ -63,9 +63,23 @@ if (window.innerWidth < laptopWidth) {
 //food menu lightbox
 //food menu lightbox on mobile
 if (window.innerWidth < laptopWidth) {
+	function FMItemOpen(elm) {
+		document.body.style.overflow = "hidden";
+		elm.classList.add("active");
+		FMOverlay.classList.add("active");
+		setTimeout(function () {
+			let elmHeight = elm.offsetHeight;
+			let elmOffsetTop = elm.offsetTop;
+			let screenHeight = window.innerHeight;
+			let scrollY = window.scrollY;
+			let newLocation =
+				(screenHeight - elmHeight) / 2 + scrollY - elmOffsetTop;
+			elm.style.marginTop = newLocation + "px";
+			elm.style.marginBottom = -newLocation + "px";
+		}, 250);
+	}
 	function FMItemclose() {
 		let elm = document.querySelector(".FM-item.active");
-		console.log(elm);
 		elm.style.marginTop = null;
 		elm.style.marginBottom = null;
 		FMOverlay.classList.remove("active");
@@ -76,34 +90,56 @@ if (window.innerWidth < laptopWidth) {
 			document.body.style.overflow = null;
 		}, 500);
 	}
+	const FMItems = document.querySelectorAll(".FM-item");
+	const FMOverlay = document.getElementById("FM-overlay");
+	for (let FMItem = 0; FMItem < FMItems.length; FMItem++) {
+		const elm = FMItems[FMItem];
+		elm.addEventListener("click", () => {
+			if (!elm.classList.contains("active")) {
+				FMItemOpen(elm);
+			}
+		});
+	}
+	FMOverlay.addEventListener("click", FMItemclose);
+}
+//food menu lightbox desktop
+else {
 	function FMItemOpen(elm) {
-		document.body.style.overflow = "hidden";
 		elm.classList.add("active");
 		FMOverlay.classList.add("active");
-		setTimeout(function () {
-			let elmHeight = elm.offsetHeight;
-			let elmOffsetTop = elm.offsetTop;
-			let screenHeight = screen.height;
-			let scrollY = window.scrollY;
-			let newLocation =
-				(screenHeight - elmHeight) / 2 + scrollY - elmOffsetTop;
-			elm.style.marginTop = newLocation + "px";
-			elm.style.marginBottom = -newLocation + 32 + "px";
-		}, 250);
+		let elmHeight = elm.offsetHeight;
+		let elmWidth = elm.offsetWidth;
+		let elmOffsetTop = elm.getBoundingClientRect().top;
+		let elmOffsetLeft = elm.getBoundingClientRect().left;
+		let screenHeight = window.innerHeight;
+		let screenWidth = window.innerWidth;
+		let XLocation = (screenWidth - elmWidth) / 2 - elmOffsetLeft;
+		let yLocation = (screenHeight - elmHeight) / 2 - elmOffsetTop;
+		elm.style.transform =
+			"translateY(" +
+			yLocation +
+			"px) translateX(" +
+			XLocation +
+			"px) scale(1.4)";
 	}
+	function FMItemclose() {
+		let elm = document.querySelector(".FM-image.active");
+		elm.style.transform = null;
+		FMOverlay.classList.remove("active");
+		elm.classList.remove("active");
+	}
+	const FMItems = document.querySelectorAll(".FM-image");
+	const FMOverlay = document.getElementById("FM-overlay");
+	for (let FMItem = 0; FMItem < FMItems.length; FMItem++) {
+		const elm = FMItems[FMItem];
+		elm.addEventListener("click", () => {
+			if (!elm.classList.contains("active")) {
+				FMItemOpen(elm);
+			}
+		});
+	}
+	FMOverlay.addEventListener("click", FMItemclose);
 }
-//food menu lightbox event
-const FMItems = document.querySelectorAll(".FM-item");
-const FMOverlay = document.getElementById("FM-overlay");
-for (let FMItem = 0; FMItem < FMItems.length; FMItem++) {
-	const elm = FMItems[FMItem];
-	elm.addEventListener("click", () => {
-		if (!elm.classList.contains("active")) {
-			FMItemOpen(elm);
-		}
-	});
-}
-FMOverlay.addEventListener("click", FMItemclose);
 //food menu filter
 if (window.innerWidth >= laptopWidth) {
 	document.getElementById("FM-filter-input").remove();
@@ -115,7 +151,6 @@ const FMFilterInput = document.getElementById("FM-filter-input");
 FMFilterInput.addEventListener("keyup", FMFilterItem);
 function FMFilterItem() {
 	let value = FMFilterInput.value.toUpperCase();
-	console.log(value);
 	let FMItems = document.querySelectorAll(".FM-item");
 	for (let item = 0; item < FMItems.length; item++) {
 		const elm = FMItems[item];
